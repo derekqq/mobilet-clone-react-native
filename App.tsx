23b6eca7709c48e20452841a94d7d19d7e204061
Dariusz Jargus <darekjargus@gmail.com>
2021-05-18 23:47:26 +0200

    added first screen, create first shared components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as S from "./Wrapper.styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <S.WrapperSafeAreaView>
+      <S.WrapperNav>
+        <S.TextNav>Stan konta</S.TextNav>
+      </S.WrapperNav>
+      <S.WrapperView>
+        <Text>dasdsadad</Text>
+      </S.WrapperView>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </S.WrapperSafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/shared/Text.styles.ts
@@ -0,0 +1,32 @@
+import { Text } from "react-native";
+import styled, { css } from "styled-components";
+
+export const BaseText = styled(Text)<TPropsText>`
+  color: #000;
+
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: bold;
+    `}
+  ${({ white }) =>
+    white &&
+    css`
+      color: #fff;
+    `}
+`;
+
+export const Text16 = styled(BaseText)<TPropsText>`
+  font-size: 16px;
+`;
+export const Text18 = styled(BaseText)<TPropsText>`
+  font-size: 18px;
+`;
+export const Text20 = styled(BaseText)<TPropsText>`
+  font-size: 20px;
+`;
+
+type TPropsText = {
+  white?: boolean;
+  bold?: boolean;
+};
